401b2a739ae89cb978394cfa5f4a282f1ed310a0
Rohit Kumar <rohitkumar703314@gmail.com>
2022-01-20 01:08:22 +0530

    Adding express validator for expenses

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json())
 
 
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.get('/', (req, res) => {
     res.send('App is Working Fine')
