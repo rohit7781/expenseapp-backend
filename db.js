8fea81850b730826e3e296f111f64d3303a95dde
Rohit Kumar <rohitkumar703314@gmail.com>
2022-01-21 16:11:19 +0530

    Adding local database url

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
 
+// mongoURI = "mongodb://localhost:27017/expenseapp?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = () => {
     mongoose.connect(process.env.mongoURI, () => {
